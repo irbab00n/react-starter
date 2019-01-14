c5b73b4771b048fc6df9ec76c1f905e9562aed15
Thomas Cosby <cosbyts@gmail.com>
2019-01-14 10:00:41 -0800

    Prune methods from SkillCard

--- client/src/components/SkillCarousel/SkillCard.jsx
+++ client/src/components/SkillCarousel/SkillCard.jsx
@@ -3,36 +3,11 @@ import React from 'react';
 export default class SkillCard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      orientationListener: false
-    };
-    this.buildClassList = this.buildClassList.bind(this);
-    this.calculateCardWidth = this.calculateCardWidth.bind(this);
-  }
-
-  buildClassList(isPortrait, isMobile) {
-    let heightString = 'about-half-ch'; // default size is half content height
-    isMobile ? heightString = 'about-full-ch' : null;
-    isPortrait ? heightString = 'about-half-ch' : null;
-    // isMobile is about-full-ch skillcard
-    // isPortrait is about-half-ch skillcard
-    return `${heightString} skill-card`;
-  }
-
-  calculateCardWidth(isPortrait, isMobile) {
-    let finalWidth = 100 / 3; // starts off at 33.3333%
-    // isMobile ? finalWidth = 50 : null; // if mobile, default to 50% for landscape
-    // isPortrait ? finalWidth = 100 : null; // if portrait, set width to 100%
-    return {minWidth: `${finalWidth}%`};
   }
 
   render() {
 
-    // let { isPortrait, isMobile } = this.props;
     let { description, image, title } = this.props.skill;
-
-    // console.log('built class list: ', this.buildClassList(isPortrait, isMobile));
-    // console.log('calculated card width: ', this.calculateCardWidth(isPortrait, isMobile));
 
     return (
       <div className={`skill-card`}>
@@ -56,5 +31,3 @@ export default class SkillCard extends React.Component {
     );
   }
 }
-
-// style={this.calculateCardWidth(isPortrait, isMobile)}
