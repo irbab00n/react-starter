7f3b299ac8c79c3c0fe4e0ec7f08a4b71217298c
Thomas Cosby <cosbyts@gmail.com>
2019-01-15 18:37:01 -0800

    Add tech stack icons to Project Panel

--- client/src/components/ProjectPanel/index.jsx
+++ client/src/components/ProjectPanel/index.jsx
@@ -5,6 +5,19 @@ import { isMobile } from 'react-device-detect';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
+const images = {
+  'aws': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/aws-ec2.png',
+  'express': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/express-bw-200.png',
+  'heroku': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/heroku-500.png',
+  'js': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/jsLogo_619x619.jpg',
+  'node': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/nodejs-512.png',
+  'postgres': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/postgres.png',
+  'react': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/react_512x512.png',
+  'react-router': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/react-router.png',
+  'sass': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/sass-256.png',
+  'webpack': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/webpack.png'
+};
+
 export default class ProjectPanel extends React.Component {
   constructor(props) {
     super(props);
@@ -45,26 +58,34 @@ export default class ProjectPanel extends React.Component {
     const { currentIndex, fadeOut } = nextProps;
     let updatedState = {};
     
+    // If the fadeOut from the parent toggles true
     if (fadeOut === true && fadeOut !== this.props.fadeOut) {
-      updatedState.showContent = false;
+      updatedState.showContent = false; // toggle off the content
     }
 
+    // If the currentIndex from the parent changes
     if (currentIndex !== this.props.currentIndex) {
-      updatedState.showContent = true;
-      ReactDOM.findDOMNode(this).scrollTop = 0;
+      updatedState.showContent = true; // toggle the content on
+      ReactDOM.findDOMNode(this).scrollTop = 0; // scroll the element back to the top
     }
 
+    // Detects if state needs to be updated
     if (Object.keys(updatedState).length > 0) {
       this.setState(updatedState);
     }
 
+    // Component will always re-render when props change
     return true;
   }
 
-  buildTechLabelImages(images) {
-    return images.map((image, index) => {
+  /* Goes through the tags and matches the appropriate image for the tag */
+  buildTechLabelImages(imageTags) {
+    return imageTags.map((imageTag, index) => {
       return (
-        <li key={`tech-label-image-${index}`}>{image}</li>
+        <li key={`tech-label-image-${index}`}>
+          {/* Look up the tag in the image cache, pull out the url, set as img src */}
+          <img src={images[imageTag]}/>
+        </li>
       );
     });
   }
@@ -120,7 +141,7 @@ export default class ProjectPanel extends React.Component {
           </div>
         </div>
 
-        <div className="inner-wrapper full-width jcc dark-bg no-padding">
+        <div className="inner-wrapper full-width jcc light-bg no-padding">
           {this.renderCarousel(project.pictures)}
         </div>
 
@@ -129,8 +150,8 @@ export default class ProjectPanel extends React.Component {
         <div className="inner-wrapper no-padding">
           <div className="project-split-wrapper">
             <div className="project-links-wrapper">
-              <div className="link-line"><i className="fas fa-link"></i><span>{project.link}</span></div>
-              <div className="link-line"><i className="fab fa-github"></i><span>{project.repo}</span></div>
+              <div className="link-line"><i className="fas fa-link"></i><a href={project.link}>{project.link}</a></div>
+              <div className="link-line"><i className="fab fa-github"></i><a href={project.repo}>{project.repo}</a></div>
             </div>
             <div className="project-contributors-wrapper">
               {project.contributors}
