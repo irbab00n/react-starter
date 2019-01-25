9f87c400d70dff99424a7837bb9c143ff4a03529
Thomas Cosby <cosbyts@gmail.com>
2019-01-24 23:22:16 -0800

    Add drop-down selectors for both the Categories and Tags

--- client/src/components/Blog/SideTrack/Tags.jsx
+++ client/src/components/Blog/SideTrack/Tags.jsx
@@ -6,14 +6,49 @@ import dispatchMappedActions from '../../../redux/dispatchMappedActions';
 class Tags extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showList: false
+    };
+    this.buildTagsList = this.buildTagsList.bind(this);
+    this.toggleShowList = this.toggleShowList.bind(this);
+  }
+
+  buildTagsList() {
+    const { storage } = this.props.views.blog.tags;
+
+    return storage.map(tag => {
+      return (
+        <li key={`tag-${tag.slug}`}>{tag.name}</li>
+      );
+    });
+  }
+
+  toggleShowList() {
+    this.setState({
+      showList: !this.state.showList
+    });
   }
 
   render() {
+    const { showList } = this.state;
 
     return (
 
       <div className="inner-wrapper tags-wrapper">
-        Tags
+        <h4>See Posts Tagged With</h4>
+        <div className="dropdown-wrapper">
+          <div className="selection-arrow-wrapper" onClick={this.toggleShowList}>
+            <span className="label">Tag</span>
+            <i className={`fas fa-angle-down arrow ${showList ? 'show' : ''}`}></i>
+          </div>
+          <ul className={showList ? 'show' : ''}>
+            {
+              showList ?
+                this.buildTagsList() :
+                null
+            }
+          </ul>
+        </div>
       </div>
 
     );
