386d0a5fc8b30ef114a7318009f18eb0f855f26d
Thomas Cosby <cosbyts@gmail.com>
2019-01-27 17:52:08 -0800

    Add additional projects

--- client/src/components/ProjectPanel/index.jsx
+++ client/src/components/ProjectPanel/index.jsx
@@ -13,9 +13,11 @@ const images = {
   'Heroku': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/heroku-500.png',
   'JS': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/jsLogo_619x619.jpg',
   'jQuery': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/jquery.png',
+  'MongoDB': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/mongodb.png',
   'Node': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/nodejs-512.png',
   'Postgres': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/postgres.png',
   'React': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/react_512x512.png',
+  'Redux': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/redux.png',
   'React-Router': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/react-router.png',
   'Sass': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/sass-256.png',
   'Webpack': 'https://s3-us-west-1.amazonaws.com/cos-bytes.com/webpack.png'
