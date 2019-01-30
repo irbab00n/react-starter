96b8ba0e0ae29aaf1cdab4ce2b70e24aaca04288
Thomas Cosby <cosbyts@gmail.com>
2019-01-29 18:34:56 -0800

    Refactor slides into externally managed module

--- client/src/components/Jumbotron/index.jsx
+++ client/src/components/Jumbotron/index.jsx
@@ -1,46 +1,7 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
 import JumbotronSlide from './JumbotronSlide';
 import SlideSelectorList from './SlideSelectorList';
-
-const slides = [
-  {
-    feature: (show) => (
-      <div className={`jumbotron-slide-container ${show ? 'show' : ''} ac jc`}>
-        <a><button className="large-font">Button 1</button></a>
-        <a><button className="large-font">Button 2</button></a>
-        <a><button className="large-font">Button 3</button></a>
-      </div>
-    ),
-    backgroundColor: '#0F6DB4',
-    subtitle: 'Example sub-title',
-    title: 'Example Title',
-    textWhite: true
-  },
-  {
-    feature: (show) => (
-      <div className={`jumbotron-slide-container ${show ? 'show' : ''} ac jc`}>
-        <a><button className="large-font">Action Button</button></a>
-      </div>
-    ),
-    backgroundColor: '#82B74F',
-    subtitle: 'Place your advertisement here!',
-    title: 'Are you selling something?',
-    textWhite: false
-  },
-  {
-    feature: (show) => (
-      <div className={`jumbotron-slide-container ${show ? 'show' : ''} ac jc`}>
-        <a><button className="large-font">Action Button 2</button></a>
-      </div>
-    ),
-    backgroundColor: '#EE6534',
-    subtitle: 'Place your advertisement here!',
-    title: 'Are you selling something?',
-    textWhite: false
-  },
-];
 
 export default class Jumbotron extends React.Component {
   constructor(props) {
@@ -53,7 +14,7 @@ export default class Jumbotron extends React.Component {
     this.setCurrentIndex = this.setCurrentIndex.bind(this);
   }
 
-  formatSlides(currentIndex, fadeOut) {
+  formatSlides(slides, currentIndex, fadeOut) {
     return slides.map((slide, index) => {
       return (
         <JumbotronSlide
@@ -88,6 +49,7 @@ export default class Jumbotron extends React.Component {
   }
 
   render() {
+    const { slides } = this.props;
     const { currentIndex, fadeOut } = this.state;
     /*
       This method invocation is using a locally created 'slides' collection.
@@ -96,7 +58,7 @@ export default class Jumbotron extends React.Component {
         1. Pass the slides in as an argument to 'formatSlides'
         2. Extract the slides from the props
     */
-    let formattedSlides = this.formatSlides(currentIndex, fadeOut);
+    let formattedSlides = this.formatSlides(slides, currentIndex, fadeOut);
 
     return (
       <section className={`jumbotron-wrapper`} style={this.setBackgroundColor(slides[currentIndex])}>
--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 import Footer from '../components/Footer/';
 import Jumbotron from '../components/Jumbotron/';
 
+import slides from '../lib/jumbotronSlides';
+
 const Home = (props) => (
   <main className="main-page-layout">
 
     {/* Jumbotron */}
-    <Jumbotron />
+    <Jumbotron
+      slides={slides}
+    />
 
-
-    
     <Footer />
 
 
